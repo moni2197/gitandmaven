29eef02ef0a11543b3e2949f0ca28d0d5324d8e3
Mohnish <rsronstones@gmail.com>
2019-11-05 14:28:12 +0530

    File Committed

---
+++ Welcome.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+import java.util.*;
+
+public class Welcome {
+	 static int i;
+		public static void main(String[] args) {
+			System.out.println("Java starts...");
+		
+
+			int x=10;
+			if(x==5)
+			{
+				System.out.println("x is 5");
+				System.out.println("inside if ");
+			}
+			else
+			{
+				System.out.println("x is 10");
+				System.out.println("inside else ");
+			}
+
+			
+		}
+}
